cfbc0a25ce79cb30234e3a646bfbc472e162b554
Isak Ruas <38894795+isakruas@users.noreply.github.com>
2021-10-17 14:38:26 -0300

    0.0.1.dev2

--- ecdh.py
+++ ecdh.py
@@ -1,11 +1,13 @@
 # https://en.wikipedia.org/wiki/Elliptic-curve_Diffie-Hellman
 import os
-from ecc import trapdoor, G
+from ecc import ECC
 
+
+ecc = ECC()
 
 rute_private_key = int.from_bytes(os.urandom(32), byteorder='little')
 
-rute_public_key = trapdoor(G, rute_private_key)
+rute_public_key = ecc.trapdoor(ecc.G, rute_private_key)
 
 print('-'*50)
 print('Chave privada de Rute:', rute_private_key)
@@ -15,7 +17,7 @@ print('Chave pública de Rute:', rute_public_key)
 
 sibele_private_key = int.from_bytes(os.urandom(32), byteorder='little')
 
-sibele_public_key = trapdoor(G, sibele_private_key)
+sibele_public_key = ecc.trapdoor(ecc.G, sibele_private_key)
 
 print('-'*50)
 print('Chave privada de Sibele:', sibele_private_key)
@@ -23,12 +25,12 @@ print('Chave privada de Sibele:', sibele_private_key)
 print('-'*50)
 print('Chave pública de Sibele:', sibele_public_key)
 
-rute_shared_key = trapdoor(sibele_public_key, rute_private_key)
+rute_shared_key = ecc.trapdoor(sibele_public_key, rute_private_key)
 
 print('-'*50)
 print('Chave compartilhada de Rute:', rute_shared_key)
 
-sibele_shared_key = trapdoor(rute_public_key, sibele_private_key)
+sibele_shared_key = ecc.trapdoor(rute_public_key, sibele_private_key)
 
 print('-'*50)
 print('Chave compartilhada de Sibele:', sibele_shared_key)
