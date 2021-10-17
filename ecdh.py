7ccca99bff1c61bdc60f3f75ec8f5eb5f1eaba0e
Isak Ruas <38894795+isakruas@users.noreply.github.com>
2021-10-17 10:26:44 -0300

    0.0.1.dev1
    > Implementação do algoritmo Diffie-Hellman

---
+++ ecdh.py
@@ -0,0 +1,39 @@
+# https://en.wikipedia.org/wiki/Elliptic-curve_Diffie-Hellman
+import os
+from ecc import trapdoor, G
+
+
+rute_private_key = int.from_bytes(os.urandom(32), byteorder='little')
+
+rute_public_key = trapdoor(G, rute_private_key)
+
+print('-'*50)
+print('Chave privada de Rute:', rute_private_key)
+
+print('-'*50)
+print('Chave pública de Rute:', rute_public_key)
+
+sibele_private_key = int.from_bytes(os.urandom(32), byteorder='little')
+
+sibele_public_key = trapdoor(G, sibele_private_key)
+
+print('-'*50)
+print('Chave privada de Sibele:', sibele_private_key)
+
+print('-'*50)
+print('Chave pública de Sibele:', sibele_public_key)
+
+rute_shared_key = trapdoor(sibele_public_key, rute_private_key)
+
+print('-'*50)
+print('Chave compartilhada de Rute:', rute_shared_key)
+
+sibele_shared_key = trapdoor(rute_public_key, sibele_private_key)
+
+print('-'*50)
+print('Chave compartilhada de Sibele:', sibele_shared_key)
+
+
+print('-'*50)
+print('As chaves compartilhadas de Sibele e Rute são as mesmas?:', sibele_shared_key == rute_shared_key)
+print('-'*50)
